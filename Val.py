d3d51588c65c37d8f48057c39688d3c2d9f08b7d
havacy <96433789+havacy@users.noreply.github.com>
2024-03-28 20:58:46 -0400

    Update Val.py

--- Val.py
+++ Val.py
@@ -1,8 +1,11 @@
 import customtkinter 
+import time 
 
 base = customtkinter.CTk()
-base.geometry("700x300")
-base.resizable("0")
+
+base.geometry("380x240")
+base.title("Nexus Login")
+base.resizable(False, False)
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
@@ -18,16 +21,13 @@ frame.pack(pady=20, padx=60, fill="both", expand=True)
 label = customtkinter.CTkLabel(master=frame, text="Login", font=("Impact", 24))
 label.pack(pady=12, padx=10)
 
-entry1 =customtkinter.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack(pady=12, padx=10)
-
-entry2 =customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
-entry2.pack(pady=12, padx=10)
+entry =customtkinter.CTkEntry(master=frame, placeholder_text="Key")
+entry.pack(pady=12, padx=10)
 
 button = customtkinter.CTkButton(master=frame, text="Login", command=login)
 button.pack(pady=12, padx=10)
 
-check = customtkinter.CTkCheckBox(master= frame, text="Remeber Me")
-check.pack(pady=12, padx=10)
 base.mainloop()
+
+
 
